8cfff6e125207d246104df90691632e1e9133e32
Suprithactr <suprithacg1@gmail.com>
2024-12-23 16:06:36 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "supritha rani";
+        return "supritha";
     }
 
 }
